7274fbe3cb7761007cb35b0de63d744b7b7bae26
mg33550336 <maciej1gruca@gmail.com>
2026-09-17 08:01:20 +0200

    update topbar, added logout function

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,6 +1,27 @@
 <script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 import { Home, User } from 'lucide-vue-next'
+import { useAuthStore } from '@/stores/auth'
+
+const router = useRouter()
+const authStore = useAuthStore()
+
+const isSigningOut = ref(false)
+
+async function handleSignOut() {
+  isSigningOut.value = true
+  try {
+    await authStore.signOut()
+    router.push({ name: 'signin' })
+  } catch (err) {
+    console.error(err.message)
+  } finally {
+    isSigningOut.value = false
+  }
+}
 </script>
+
 <template>
     <div class="flex justify-between pb-2">
         <router-link
@@ -9,9 +30,14 @@ import { Home, User } from 'lucide-vue-next'
             <Home class="w-6 h-6 text-text"/>
         </router-link>
 
-        <div class="w-12 h-12 rounded-card bg-blue border border-border flex items-center justify-center">
+        <button
+            type="button"
+            :disabled="isSigningOut"
+            class="w-12 h-12 rounded-card bg-blue border border-border flex items-center justify-center disabled:opacity-60"
+            @click="handleSignOut"
+        >
             <User class="w-6 h-6 text-text"/>
-        </div>
+        </button>
 
     </div>
 </template>
